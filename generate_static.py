e99be99bfd86040053aeb1afab412d2a17bba173
noah-bakken <noahbakken477@gmail.com>
2025-08-17 22:23:34 +0200

    vercel lets go

--- generate_static.py
+++ generate_static.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for
 import os
 import shutil
-from app import app, TRAVEL_DATA, COUNTRY_ORDER
+from app import app, TRAVEL_DATA, COUNTRY_ORDER, get_images_in_folder
 
 def generate_static():
     # Create static directory if it doesn't exist
@@ -14,8 +14,23 @@ def generate_static():
     
     # Generate index.html
     with app.test_request_context():
+        # Prepare countries with main image and custom ordering
+        countries_with_images = {}
+        for country, data in TRAVEL_DATA.items():
+            country_path = os.path.join('static', 'photos', country)
+            images = get_images_in_folder(country_path)
+            countries_with_images[country] = {
+                **data,
+                'main_image': images[0] if images else None
+            }
+
+        sorted_countries = {}
+        for country in COUNTRY_ORDER:
+            if country in countries_with_images:
+                sorted_countries[country] = countries_with_images[country]
+
         # Generate home page
-        html = render_template('index.html', countries=TRAVEL_DATA)
+        html = render_template('index.html', countries=sorted_countries)
         with open('static_site/index.html', 'w', encoding='utf-8') as f:
             f.write(html)
         
@@ -32,7 +47,8 @@ def generate_static():
             
             # Generate city pages
             for city in TRAVEL_DATA[country]['cities']:
-                city_dir = f'{country_dir}/{city}'
+                # Match dynamic route structure: /city/<country>/<city>
+                city_dir = f'static_site/city/{country}/{city}'
                 os.makedirs(city_dir, exist_ok=True)
                 
                 # Get images for this city
@@ -52,9 +68,35 @@ def generate_static():
         
         # Generate about and contact pages
         for page in ['about', 'contact']:
+            page_dir = f'static_site/{page}'
+            os.makedirs(page_dir, exist_ok=True)
             html = render_template(f'{page}.html')
-            with open(f'static_site/{page}/index.html', 'w', encoding='utf-8') as f:
+            with open(f'{page_dir}/index.html', 'w', encoding='utf-8') as f:
                 f.write(html)
+
+        # Generate map page
+        map_dir = 'static_site/map'
+        os.makedirs(map_dir, exist_ok=True)
+        visited_countries = [
+            "Spain", "Iceland", "Morocco", "Croatia", "Greece", "Portugal", "Hungary",
+            "United Kingdom", "Germany", "Netherlands", "Austria", "Slovakia", "Czech Republic",
+            "Israel", "Denmark", "Norway", "Poland", "Turks and Caicos", "Canada", "Costa Rica",
+            "United States"
+        ]
+        bucket_list_countries = [
+            "Turkey",
+            "Japan",
+            "Singapore",
+            "United Arab Emirates",
+            "Switzerland",
+            "United Republic of Tanzania",
+            "New Zealand",
+            "Australia",
+            "Madagascar",
+        ]
+        html = render_template('map.html', visited_countries=visited_countries, bucket_list_countries=bucket_list_countries)
+        with open(f'{map_dir}/index.html', 'w', encoding='utf-8') as f:
+            f.write(html)
 
 if __name__ == '__main__':
     generate_static() 
